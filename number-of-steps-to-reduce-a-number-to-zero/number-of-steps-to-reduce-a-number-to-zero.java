97235dbc4bcf9e9fcb0bfc82d1fed3e26ab2c691
debayangg <66942246+debayangg@users.noreply.github.com>
2023-06-01 14:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (45.98%) - LeetSync

--- number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps[]= new int[num+1];
-        steps[0]=0;
-        for(int i=1;i<=num;i++)
+        int count=0;
+        
+        for(int i=num;i!=0;)
         {
-            int min=steps[i-1];
-            
-            if(i%2==0 && min>steps[i/2])
-            min=steps[i/2];
-
-            steps[i]=1+min;
+            count++;
+            if(i%2==0)
+            i/=2;
+            else
+            i--;
         }
 
-        return steps[num];
+        return count;
     }
 }
