37f265865fda5854f7ef14dc45e2ffb26ae4089c
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-06-04 14:11:21 +0530

    Time: 3 ms (74.70%) | Memory: 41.5 MB (82.92%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,26 +1,27 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int n=s.length();
-        HashMap<Character,Integer> m = new HashMap<>();
-        for(int i=0;i<n;i++)
-        {
-            char ch=s.charAt(i);
-            if(m.containsKey(ch))
-            {
-                m.put(ch,m.get(ch)+1);
-            }
-            else
-            {
-                m.put(ch,1);
+        int n = s.length();
+        int[] lowerCount = new int[26]; 
+        int[] upperCount = new int[26]; 
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                lowerCount[ch - 'a']++;
+            } else {
+                upperCount[ch - 'A']++;
             }
         }
-        int count=0;
-        for(char ch : m.keySet())
-        {
-            count+=2*(m.get(ch)/2);
+
+        int count = 0;
+        for (int freq : lowerCount) {
+            count += 2 * (freq / 2); 
         }
-        if(n>count)
-        {
+        for (int freq : upperCount) {
+            count += 2 * (freq / 2);
+        }
+
+        if (n > count) {
             count++;
         }
 
