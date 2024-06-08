44b0322d7c4b1b1bea0290a462a21cd94a9a844d
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-06-08 10:33:23 +0530

    Time: 285 ms (5.06%) | Memory: 137.8 MB (12.64%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         vector<int> num;
-        map<long long,set<int>> m;
+        map<long long,int> m;
         long long sum=0;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
             if(sum%k==0 && i>0)return true;
-            m[sum%k].insert(i);
+            if(!m[sum%k])
+            m[sum%k]=i+1;
         }
         sum=0;
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
             int val=sum%k;
-            auto it = lower_bound(m[val].begin(),m[val].end(),i);
-            if(it!=m[val].begin() && i-*(m[val].begin())>1)
+            if(i-m[val]>0)
             return true;
         }
         return false;
