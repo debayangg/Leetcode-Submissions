81d1bcc3ca74a393ce49e0caf0dad8617563e118
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-06-08 10:35:43 +0530

    Time: 189 ms (34.37%) | Memory: 140.1 MB (6.19%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -9,13 +9,7 @@ public:
             if(sum%k==0 && i>0)return true;
             if(!m[sum%k])
             m[sum%k]=i+1;
-        }
-        sum=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum+=nums[i];
-            int val=sum%k;
-            if(i-m[val]>0)
+            if(i-m[sum%k]>0)
             return true;
         }
         return false;
