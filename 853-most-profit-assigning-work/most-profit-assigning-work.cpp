68f9155fd3fa924e3fbbc2bff7fb69c2230612aa
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-06-18 15:57:39 +0530

    Time: 95 ms (22.94%) | Memory: 44.2 MB (25.52%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        vector<pair<int,int>> arr;
+        int n=profit.size();
+        for(int i=0;i<n;i++)
+        {
+            arr.push_back({profit[i],difficulty[i]});
+        }
+        sort(arr.rbegin(),arr.rend());
+        sort(worker.begin(),worker.end());
+        int mx=worker.size()-1,ans=0;
+        for(int i=0;i<n;i++)
+        {
+            auto it=lower_bound(worker.begin(),worker.end(),arr[i].second);
+            if(it!=worker.end() && distance(worker.begin(),it)<=mx)
+            {
+                auto en=worker.begin();
+                advance(en,mx);
+                ans+=(arr[i].first)*(distance(it,en)+1);
+                mx=distance(worker.begin(),it)-1;
+                cout<<i<<" "<<*it<<" "<<mx<<endl;
+                cout<<ans<<endl;
+            }
+        }
+        return ans;
+    }
+};
