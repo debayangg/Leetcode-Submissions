ddca7c88443084f8be89cc2b610b90408a846329
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-07-09 10:31:25 +0530

    Time: 2 ms (42.81%) | Memory: 7.2 MB (87.14%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        memset(dp,-1,sizeof dp);
+        for(int i=0;i<m;i++)
+        {
+            if(i==0)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    dp[i][j]=1;
+                }
+            }
+            else
+            dp[i][0]=1;
+        }
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
