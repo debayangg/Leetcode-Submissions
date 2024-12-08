91a7a24afe6a0bfe7345aae8cd2e00bb58fc5ab9
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-08 18:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (100.00%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
     int findMinimumTime(vector<int>& strength, int K) {
-        int n=strength.size();
-        int bitmask = 0;
-        int dp[n+1][(1<<n)];
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<(1<<n);j++)
-            {
-                dp[i][j]=INT_MAX;
-            }
-        }
+        int n = strength.size();
+        vector<long> dp(1 << n, LONG_MAX); // Initialize DP table
+        dp[0] = 0; // Base case: No workers, no time
 
-        dp[0][0]=0;
-        int X=1;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=0;j<(1<<n);j++)
-            {
-                if(dp[i-1][j]!=INT_MAX)
-                {
-                    int bitmask = j;
-                    for(int j=0;j<n;j++)
-                    {
-                        if(!(bitmask&(1<<j)))
-                        {
-                            int curr_X=X+(i-1)*K;
-                            int new_bitmask = (bitmask|(1<<j));
-                            dp[i][new_bitmask]=min(dp[i][new_bitmask],
-                            (strength[j]+curr_X-1)/curr_X + dp[i-1][bitmask]);
-                        }
-                    }
+        for (int i = 0; i < (1 << n); ++i) { // Iterate over all subsets
+            int f = 1 + __builtin_popcount(i) * K; // Efficiency factor for current subset
+            for (int j = 0; j < n; ++j) { // Try adding a new worker to the subset
+                if (!(i & (1 << j))) { // Worker j not in subset
+                    long time = (strength[j] + f - 1) / f; // Calculate time for worker j
+                    dp[i | (1 << j)] = min(dp[i | (1 << j)], dp[i] + time); // Update DP state
                 }
             }
         }
-        return dp[n][(1<<n)-1];
+        
+        return dp[(1 << n) - 1] < LONG_MAX ? dp[(1 << n) - 1] : -1; // Final result
     }
 };
