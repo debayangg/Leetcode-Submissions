1d143ae0d154a1307f58a00e171c1db4d48d90b2
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-08 16:08:21 +0530

    Time: 64 ms (100.00%) | Memory: 112.6 MB (26.92%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -2,37 +2,29 @@ class Solution {
 public:
     int findMinimumTime(vector<int>& strength, int K) {
         int n=strength.size();
-        int bitmask = 0;
-        int dp[n+1][(1<<n)];
-        for(int i=0;i<=n;i++)
+        int lim=(1<<n);
+        vector<vector<long long>>dp(lim,vector<long long>(100,INT_MAX));
+        dp[0][1]=0;
+        for(int i=0;i<lim;++i)
         {
-            for(int j=0;j<(1<<n);j++)
+            for(int j=1;j<100;++j)
             {
-                dp[i][j]=INT_MAX;
-            }
-        }
-
-        dp[0][0]=0;
-        int X=1;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=0;j<(1<<n);j++)
-            {
-                if(dp[i-1][j]!=INT_MAX)
+                if(dp[i][j]==INT_MAX)
+                    continue;
+                for(int k=0;k<n;++k)
                 {
-                    int bitmask = j;
-                    for(int j=0;j<n;j++)
+                    if(!(i&(1<<k)))
                     {
-                    if(!(bitmask&(1<<j)))
-                    {
-                        int curr_X=X+(i-1)*K;
-                        int new_bitmask = (bitmask|(1<<j));
-                        dp[i][new_bitmask]=min(dp[i][new_bitmask],(strength[j]+curr_X-1)/curr_X + dp[i-1][bitmask]);
+                        long long temp=(strength[k]+j-1)/j;
+                        int ni=i|(1<<k),nk=j+K;
+                        dp[ni][nk]=min(dp[ni][nk],dp[i][j]+temp);
                     }
                 }
-                }
             }
         }
-        return dp[n][(1<<n)-1];
-    }
+        long long ans=INT_MAX;
+        for(int i=1;i<100;++i)
+            ans=min(ans,dp[lim-1][i]);
+        return (ans<=INT_MAX)?(int)ans:-1;
+    }
 };
