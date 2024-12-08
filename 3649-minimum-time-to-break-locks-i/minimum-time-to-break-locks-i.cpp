b2e1095ffac7409481aeb6813a2cc4f402df18f4
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-08 15:50:47 +0530

    Time: 12 ms (100.00%) | Memory: 26.8 MB (100.00%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int helper(vector<int>& strength, int K, int X, int bitmask, int lim)
-    {
-        if(bitmask==lim)
-        {
-            return 0;
-        }
-
+    int findMinimumTime(vector<int>& strength, int K) {
         int n=strength.size();
-
-        int ans=INT_MAX;
-        for(int i=0;i<n;i++)
+        int bitmask = 0;
+        int dp[n+1][(1<<n)];
+        for(int i=0;i<=n;i++)
         {
-            if(!(bitmask&(1<<i)))
+            for(int j=0;j<(1<<n);j++)
             {
-                ans=min(ans,int(ceil(double(strength[i])/X))+helper(strength, K, X+K, bitmask|(1<<i), lim));
+                dp[i][j]=INT_MAX;
             }
         }
-        return ans;
-    }
-    int findMinimumTime(vector<int>& strength, int K) {
-        int n = strength.size();
-        vector<int> way;
-        int ans = helper(strength, K, 1, 0, ((1<<n)-1));
-        return ans; 
+
+        dp[0][0]=0;
+        int X=1;
+        for(int i=1;i<=n;i++)
+        {
+            vector<int> vec;
+            for(int j=0;j<(1<<n);j++)
+            {
+                if(dp[i-1][j]!=INT_MAX)vec.push_back(j);
+            }
+            for(int bitmask:vec)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(!(bitmask&(1<<j)))
+                    {
+                        dp[i][bitmask|(1<<j)]=min(dp[i][bitmask|(1<<j)],int(ceil((double)strength[j]/(X+(i-1)*K))) + dp[i-1][bitmask]);
+                    }
+                }
+            }
+        }
+        return dp[n][(1<<n)-1];
     }
 };
