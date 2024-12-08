0e011067ac2b97080cd7cff673cf603304f45c20
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-08 16:11:29 +0530

    Time: 0 ms (100.00%) | Memory: 25 MB (100.00%) - LeetSync

--- 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
+++ 3649-minimum-time-to-break-locks-i/minimum-time-to-break-locks-i.cpp
@@ -23,13 +23,14 @@ public:
                     int bitmask = j;
                     for(int j=0;j<n;j++)
                     {
-                    if(!(bitmask&(1<<j)))
-                    {
-                        int curr_X=X+(i-1)*K;
-                        int new_bitmask = (bitmask|(1<<j));
-                        dp[i][new_bitmask]=min(dp[i][new_bitmask],(strength[j]+curr_X-1)/curr_X + dp[i-1][bitmask]);
+                        if(!(bitmask&(1<<j)))
+                        {
+                            int curr_X=X+(i-1)*K;
+                            int new_bitmask = (bitmask|(1<<j));
+                            dp[i][new_bitmask]=min(dp[i][new_bitmask],
+                            (strength[j]+curr_X-1)/curr_X + dp[i-1][bitmask]);
+                        }
                     }
-                }
                 }
             }
         }
