59a8023947c743f73dd5d6730dda6e881e6798c2
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-10 01:41:49 +0530

    Time: 3 ms (96.42%) | Memory: 7.9 MB (49.91%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int countArrangement(int n) 
     {
-        vector<int> arr(n);
-        for(int i=1;i<=n;i++) arr[i-1]=i;
-
         int dp[(1<<n)];
 
         for(int i=0;i<(1<<n);i++)dp[i]=0;
@@ -17,7 +14,7 @@ public:
             {
                 if(!(i&(1<<j)))
                 {
-                    if((j+1)%arr[cnt]==0 || arr[cnt]%(j+1)==0)
+                    if((j+1)%(cnt+1)==0 || (cnt+1)%(j+1)==0)
                     dp[i|(1<<j)] += dp[i];
                 }
             }
