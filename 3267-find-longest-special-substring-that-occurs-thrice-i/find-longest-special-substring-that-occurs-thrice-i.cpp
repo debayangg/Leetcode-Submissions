fa0f43b68c7c4ea9af770de249721f092267d252
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2024-12-10 13:48:51 +0530

    Time: 11 ms (63.29%) | Memory: 15.2 MB (26.98%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int maximumLength(string s) 
+    {
+        int n=s.size();
+        map<char,map<int,int>> mp;
+        vector<int> ans;
+        int cnt=1;
+        for(int i=1;i<s.size();i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                cnt++;
+            }
+            else
+            {
+                mp[s[i-1]][cnt]++;
+                cnt=1;
+            }
+        }
+        if(cnt>0)
+        mp[s[n-1]][cnt]++;
+
+        for(auto id:mp)
+        {
+            char ch=id.first;
+            map<int,int> mp1=id.second;
+            vector<int> l;
+            pair<int,int> last=*(mp1.rbegin());
+
+            for(int i=last.first;i>=1;i--)
+            {
+                l.push_back(i);
+            }
+
+            for(int x:l)
+            {
+                int occurence = mp1[x];
+                for(int i=x-1;i>=1;i--)
+                {
+                    mp1[i]+=(x-i+1)*occurence;
+                }
+                if(occurence>2)
+                {
+                    ans.push_back(x);
+                    break;
+                }
+            }
+        }
+        sort(ans.rbegin(),ans.rend());
+        return (ans.size())?ans[0]:-1;
+    }
+};
