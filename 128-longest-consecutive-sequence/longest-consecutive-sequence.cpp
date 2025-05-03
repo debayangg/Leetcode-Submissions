761f8112ac83005e70604ccd6857307d0ccb8059
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-05-03 22:09:17 +0530

    Time: 613 ms (5.01%) | Memory: 148.3 MB (5.17%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        int n=arr.size(),mx=0;
+        for(int i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        unordered_map<int,int> vis;
+        unordered_map<int,int> vals;
+        for(auto id:arr)
+        {
+            if(vis[id])continue;
+
+            int i=id,cnt=0;
+            cout<<i<<endl;
+            while(mp[i])
+            {
+                vis[i]=1;
+                if(vals[i])
+                {
+                    cnt+=vals[i];
+                    break;
+                }
+                cnt++;
+                i--;
+            }
+            // for(int x=0;x<=200;x++)
+            // {
+            //     if(mp[x])cout<<x<<" "<<vis[x]<<endl;
+            // }
+            mx=max(mx,cnt);
+            vals[id]=cnt;
+        }
+        return mx;
+    }
+};
