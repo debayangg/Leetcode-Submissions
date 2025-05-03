388b1ade5eb73f61850d835b28f931405fc0b9b8
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-05-03 15:28:22 +0530

    Time: 11 ms (21.55%) | Memory: 115.8 MB (5.60%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -6,18 +6,24 @@ public:
         unordered_map<int,int> top;
         unordered_map<int,int> btm;
         int n=tops.size();
-        for(int i=0;i<n;i++)
+        int val=0,a=tops[0],b=bottoms[0];
+        top[tops[0]]++;
+        btm[bottoms[0]]++;
+        bool flag=false;
+        for(int i=1;i<n;i++)
         {
-            mp[tops[i]].insert(i);
-            mp[bottoms[i]].insert(i);
             top[tops[i]]++;
             btm[bottoms[i]]++;
+            if(!(tops[i]==a || bottoms[i]==a))a=0;
+            if(!(tops[i]==b || bottoms[i]==b))b=0;
+            if(a==0 && b==0){
+                flag=true;
+                break;
+            }
         }
-        int val=0;
-        for(auto id:mp)
-        {
-            if(id.second.size()==n)val=id.first;
-        }
+        if(flag)return -1;
+
+        val=(a!=0)?a:b;
         if(val==0)
         return-1;
         else
