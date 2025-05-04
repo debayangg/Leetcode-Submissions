aa8480d5746fe650f9d7e6257327a5dda5c0bc9b
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-05-04 13:52:13 +0530

    Time: 4 ms (51.00%) | Memory: 26.1 MB (92.25%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) 
     {
-        map<vector<int>,int>m;
+        map<pair<int,int>,int>m;
         int ans=0;
         for(int i=0; i<dominoes.size(); i++)
         {
-            sort(dominoes[i].begin(),dominoes[i].end());
-            ans+=m[dominoes[i]];
-            m[dominoes[i]]++;
+            int a=dominoes[i][0], b=dominoes[i][1];
+            pair<int,int> x;
+            x.first=(a>b)?a:b;
+            x.second=(a>b)?b:a;
+            ans+=m[x];
+            m[x]++;
         }
         return ans;
         
