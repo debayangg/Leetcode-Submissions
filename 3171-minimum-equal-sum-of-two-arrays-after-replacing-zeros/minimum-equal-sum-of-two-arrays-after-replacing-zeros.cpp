d4720b75ec08b76ccfe9e01ace8a4eae0b4cd1ce
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-05-10 22:27:44 +0530

    Time: 142 ms (20.34%) | Memory: 135.5 MB (15.59%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,25 +1,58 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        long long mn1=0,mn2=0;
+        long long mn1=0,mn2=0, n1=nums1.size(), n2=nums2.size();
         int cnt1=0 , cnt2=0;
-        for(int i:nums1)
+        bool f1=true, f2=true;
+        for(int i=0,j=0;i<n1 && j<n2;)
         {
-            if(i==0){
-                cnt1++;
-                mn1+=1;
-            }
-            else mn1+=i;
-        }
-        for(int i:nums2)
-        {
-            if(i==0)
+            if(!(f1 || f2))break;
+            if(f1)
             {
-                mn2+=1;
-                cnt2++;
+                if(nums1[i]==0)
+                {
+                    cnt1++;
+                    mn1+=1;
+                }
+                else mn1+=nums1[i];
+                i++;
             }
-            else mn2+=i;
+            if(f2)
+            {
+                if(nums2[j]==0)
+                {
+                    cnt2++;
+                    mn2+=1;
+                }
+                else mn2+=nums2[j];
+                j++;
+            }
+            if(i==n1)
+            {
+                i--;
+                f1=false;
+            }
+            if(j==n2)
+            {
+                j--;
+                f2=false;
+            }
         }
+        // cout<<mn1<<" "<<cnt1<<endl;
+        // cout<<mn2<<" "<<cnt2<<endl;
+        // for(int i:nums1)
+        // {
+            
+        // }
+        // for(int i:nums2)
+        // {
+        //     if(i==0)
+        //     {
+        //         mn2+=1;
+        //         cnt2++;
+        //     }
+        //     else mn2+=i;
+        // }
         if(cnt1 && cnt2)
         {
             return max(mn1,mn2);
