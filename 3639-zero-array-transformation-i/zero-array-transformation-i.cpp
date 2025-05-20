20c74d033f836ee23ec4fe2864ca215ec60fb938
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-05-20 11:21:13 +0530

    Time: 0 ms (100.00%) | Memory: 295 MB (39.21%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int>diff(nums.size(),0);
+        int n=nums.size();
+        vector<int> delta(n);
         for(int i=0;i<queries.size();i++)
         {
-            int a=queries[i][0];
-            int b=queries[i][1];
-            diff[a]+=1;
-            if(b+1<nums.size())
-            {
-                diff[b+1]-=1;
-            }
+            int a=queries[i][0], b=queries[i][1];
+            delta[a]+=1;
+            if(b+1<n)
+            delta[b+1]-=1;
         }
-        int  curr=0;
-        for(int i=0;i<nums.size();i++)
+        vector<int> cnt(n,0);
+        cnt[0]=delta[0];
+        bool flag=true;
+        if(cnt[0]<nums[0])flag=false;
+        for(int i=1;i<n;i++)
         {
-            curr+=diff[i];
-            nums[i]-=curr;
-            if(nums[i]>0)
-            {
-                return false;
+            delta[i]+=delta[i-1];
+            cnt[i]=delta[i];
+            if(cnt[i]<nums[i]){
+                flag=false;
+                break;
             }
         }
-        return true;
+
+        return (flag)?true:false;
     }
 };
