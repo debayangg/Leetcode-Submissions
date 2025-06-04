09b75b7fa91747e87b49719d78e4024dff0ee831
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-06-05 01:31:55 +0530

    Time: 58 ms (6.78%) | Memory: 45.4 MB (49.15%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,28 +1,31 @@
 class Solution {
     public String answerString(String word, int numFriends) {
-        String s="", ans="";
-        if(numFriends==1)return word;
-        for(int i=0;i<word.length()-numFriends+1;i++)
-        {
-            s+=word.charAt(i);
-            if(ans.compareTo(s)<0)
-            ans=s;
+        StringBuilder s = new StringBuilder(), ans = new StringBuilder();
+        
+        if (numFriends == 1) return word;
+        
+        for (int i = 0; i < word.length() - numFriends + 1; i++) {
+            s.append(word.charAt(i));
+            if (ans.toString().compareTo(s.toString()) < 0) {
+                ans = new StringBuilder(s);
+            }
         }
 
-        for(int i=word.length()-numFriends+1;i<word.length();i++)
-        {
-            s=s.substring(1);
-            s+=word.charAt(i);
-            if(ans.compareTo(s)<0)
-            ans=s;
+        for (int i = word.length() - numFriends + 1; i < word.length(); i++) {
+            s.deleteCharAt(0);
+            s.append(word.charAt(i));
+            if (ans.toString().compareTo(s.toString()) < 0) {
+                ans = new StringBuilder(s);
+            }
         }
 
-        while(!s.equals(""))
-        {
-            s=s.substring(1);
-            if(ans.compareTo(s)<0)
-            ans=s;
+        while (s.length() > 0) {
+            s.deleteCharAt(0);
+            if (ans.toString().compareTo(s.toString()) < 0) {
+                ans = new StringBuilder(s);
+            }
         }
-        return ans;
+        
+        return ans.toString();
     }
 }
