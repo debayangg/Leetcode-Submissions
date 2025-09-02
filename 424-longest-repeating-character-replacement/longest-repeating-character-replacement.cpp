09e78e793d4c343f4213bc20feffb70b27093110
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-09-02 10:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (69.97%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int mp[26] = {0};
-        int i = 0, j = 0, mx = 0;
+        int mp[28];
+        int i=0,j=0,mx=0;
+        int mx1=0;
+        while(j<s.size())
+        {
+            int a = mp[s[j]-'A'];
+            char c = s[j];
+            mp[s[j]-'A']++;
 
-        while (j < s.size()) {
-            mp[s[j] - 'A']++;
-
-            // Recompute the true maximum frequency in current window
-            int mx1 = 0;
-            for (int c = 0; c < 26; c++) {
-                mx1 = max(mx1, mp[c]);
-            }
-
-            // If replacements needed > k, shrink from left
-            while ((j - i + 1) - mx1 > k) {
-                mp[s[i] - 'A']--;
+            mx1=max(mx1,mp[s[j]-'A']);
+            
+            if(j-i+1-mx1>k)
+            {
+                mp[s[i]-'A']--;
                 i++;
-
-                // Recompute max again after shrinking
-                mx1 = 0;
-                for (int c = 0; c < 26; c++) {
-                    mx1 = max(mx1, mp[c]);
-                }
             }
-
-            mx = max(mx, j - i + 1);
+            else
+            {
+                mx=max(mx,j-i+1);
+            }
             j++;
         }
         return mx;
