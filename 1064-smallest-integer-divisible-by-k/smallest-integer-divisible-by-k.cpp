4465ebb8d64cd655820e6fdbc8b29677ef927e7a
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-11-25 22:04:15 +0530

    Time: 1 ms (32.90%) | Memory: 7.7 MB (98.48%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -4,16 +4,15 @@ public:
         if(k%2==0)return -1;
         if(k%5==0)return -1;
         int n=1;
-        int ans=1, rem=0;
-        while(true)
+        int ans=0, rem=-1;
+        while(rem)
         {
+            if(rem==-1)rem=0;
             rem+=n%k;
             rem%=k;
-            if(rem==0)return ans;
             ans++;
             n=(n%k)*10;   
-            //cout<<n<<" "<<rem<<endl;
         }
-        return -1;
+        return ans;
     }
 };
