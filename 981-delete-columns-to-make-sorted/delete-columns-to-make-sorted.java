c7cd68d3096431fb33c9bb787218f6cded10a26d
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-12-22 11:54:39 +0530

    Time: 8 ms (65.14%) | Memory: 47.4 MB (16.74%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -1,21 +1,19 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
         int n = strs[0].length();
-        Set<Integer> st = new HashSet<>();
+        int count = 0;
         for(int i=0;i<n;i++)
         {
             boolean flag = false;
             for(int j=1;j<strs.length;j++)
             {
                 if(strs[j].charAt(i)<strs[j-1].charAt(i)) {
-                    flag = true;
+                    count++;
                     break;
                 }
             }
-
-            if(flag)st.add(i);
         }
 
-        return st.size();
+        return count;
     }
 }
