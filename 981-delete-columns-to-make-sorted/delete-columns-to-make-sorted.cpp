7408aaae6342dfe8ccfe6c357db3b43a4116a0b0
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2025-12-22 11:51:30 +0530

    Time: 4 ms (47.13%) | Memory: 15.7 MB (18.30%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
         int n = strs[0].size();
-        set<int> s;
+        unordered_set<int> s;
         for(int i=0;i<n;i++)
         {
             for(int j=1;j<strs.size();j++)
