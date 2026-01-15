528626e4177c86da58ab62819b3ec7f4cc3155c9
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2026-01-15 13:24:46 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (78.61%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -12,13 +12,6 @@ public:
             }
             st.push(nums[i]);
         }
-        stack<int> cp = st;
-        while(!cp.empty())
-        {
-            cout<<cp.top()<<endl;
-            cp.pop();
-        }
-
         for(int i = nums.size()-1;i>=0;i--)
         {
             while(!st.empty() && nums[i]>=st.top())
@@ -29,19 +22,7 @@ public:
             else ans[i]=-1;
 
             st.push(nums[i]);
-
-            // cp = st;
-            // cout<<"H"<<endl;
-            // while(!cp.empty())
-            // {
-            //     cout<<cp.top()<<endl;
-            //     cp.pop();
-            // }
         }
-
-        // vector<int> ans;
-        // for(int x:nums)
-        // ans.push_back(mp[x]);
 
         return ans;
     }
