3fbb53f220bb21ab48c356ff9e0b3c905f76be5c
Debayan Ghosh <66942246+debayangg@users.noreply.github.com>
2026-01-15 12:46:27 +0530

    Time: 3 ms (42.35%) | Memory: 13 MB (13.68%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mp;
+        stack<int> stk;
+
+        for(int i=nums2.size()-1;i>=0;i--)
+        {
+            while(!stk.empty() && stk.top()<=nums2[i])
+            {
+                stk.pop();
+            }
+            if(!stk.empty())
+            mp[nums2[i]]=stk.top();
+            else
+            mp[nums2[i]]=-1;
+
+            stk.push(nums2[i]);
+        }
+
+        vector<int> ans;
+        for(int x:nums1)
+        ans.push_back(mp[x]);
+
+        return ans;
+
+    }
+};
